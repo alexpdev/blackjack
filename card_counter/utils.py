42776bca393e6c1c9b067b6c176ae0a19165c751
alexpdev <alexpdev@protonmail.com>
2021-05-25 23:21:12 -0700

    updated source README and outline

--- card_counter/utils.py
+++ card_counter/utils.py
@@ -1,8 +1,25 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
 
-
-
-
+#######################################################################
+# BlackJack Card Counting
+# Copyright (C) 2021  alexpdev
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+# Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses
+#########################################################################
 hx = "#FeeFFF"
+
 def main(screen,t):
     lst = [diamond,heart,spade,club]
     for i in range(-360,640,250):
--- outline.md
+++ outline.md
@@ -1,5 +1,45 @@
 # Blackjack Outline #
 
-
 - Card class
+    1 get image path function
+    2 get value
+
+--------------------
+
 - Deck class
+    1 number of decks * 52
+    2 shuffle cards
+    3 pop card off deck
+
+--------------------
+
+- Player class
+    1 is turn function
+    2 hand = list if card values
+    3 score = sum of hand
+    4 add card function
+    5 set turn function
+
+--------------------
+
+- Window class
+    1 Main window
+    2 player groupboxes
+    3 card images
+    4 score box
+    5 output window
+    6 deck count
+    7 card count
+    8 hit, stand, new game buttons
+    9 next round button
+
+--------------------
+
+- Dealer class
+    1 Player subclass
+    2 deal card to player
+    3 deal card to self
+    4 round table for hit or stay
+    5 if hand > 15 deal self
+    6 if player or self break = lose
+    7 highest score under 22 wins
